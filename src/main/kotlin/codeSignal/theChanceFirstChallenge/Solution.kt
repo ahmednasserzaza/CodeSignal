aafc6849a992b733aca9529e68d02059aa003a23
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-01-31 16:16:30 +0200

    remove unnecessary condition

--- src/main/kotlin/codeSignal/theChanceFirstChallenge/Solution.kt
+++ src/main/kotlin/codeSignal/theChanceFirstChallenge/Solution.kt
@@ -3,7 +3,7 @@ package codeSignal.theChanceFirstChallenge
 fun solution(list: MutableList<String>): Double {
     val newList = list.filter { it == "a" || it == "b" }
     val numOfAs = list.count { it == "a" }.toDouble()
-    return if (list == newList && ("a" in list) && ("b" in list) && list.size > 1) {
+    return if ((list == newList) && ("a" in list) && ("b" in list)) {
         numOfAs / list.size
     } else -1.0
 }
