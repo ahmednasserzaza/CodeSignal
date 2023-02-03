be82acafe4173ee361ffbba2bc9ffadf232e4fd7
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-02-03 18:41:16 +0200

    new challenge

--- src/main/kotlin/codeSignal/statuesArrange/Solution.kt
+++ src/main/kotlin/codeSignal/statuesArrange/Solution.kt
@@ -1,16 +1,27 @@
 package codeSignal.statuesArrange
 
 fun solution(statues: MutableList<Int>): Int {
-    val missed: MutableList<Int> = mutableListOf()
-    val maxStatue = statues.max()
-    val miniStatue = statues.min()
-
-    for (i in miniStatue..maxStatue) {
-        if (i !in statues) missed.add(i)
+    statues.sort()
+    var count = 0
+    for (i in 1 until statues.size){
+        val different = statues[i] - statues[i-1]
+        if (different > 1) count += different -1
     }
-    return missed.size
+    return count
 }
 
 fun main() {
     println(solution(mutableListOf(6, 2, 3, 8)))
 }
+
+/*
+fun solution(statues: MutableList<Int>): Int {
+    val missed: MutableList<Int> = mutableListOf()
+    val maxStatue = statues.max()
+    val miniStatue = statues.min()
+    for (i in miniStatue..maxStatue) {
+        if (i !in statues) missed.add(i)
+    }
+    return missed.size
+}
+ */
